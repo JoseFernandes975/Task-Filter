57766b567601003b9b766c14075b6a783b90b880
JoseFernandes975 <zehenrique752005@gmail.com>
2024-01-25 15:35:14 -0300

    Layout css and component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export default function Header() {
+  return(
+    <header>
+        <nav className='ds-container'>
+            <h1>DSFilter</h1>
+            <p>6 produto(s)</p>
+        </nav>
+    </header>
+  );
+}
